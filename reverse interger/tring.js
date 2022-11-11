1d5e4c394e942cc304e8608c81d32664550aa3b2
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2022-11-11 11:41:03 +0200

    Add files via upload

---
+++ reverse interger/tring.js
@@ -0,0 +1,9 @@
+
+var reverse = function(x) {
+	
+    let reversed = Math.abs(x).toString().split('').reverse().join('');
+    
+    if(reversed > 2 ** 31 - 1 ) return 0;
+    
+    return Math.sign(x) * reversed
+};
